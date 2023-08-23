984bfa5f0f86257924743a9d90a748db29b22c7d
riskmaris <mariskaps20@gmail.com>
2023-08-23 11:37:20 +0300

    Add API

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,44 +1,115 @@
+// import './form.css';
+// import { useDispatch } from 'react-redux';
+// import { useState } from 'react';
+// import { addBook } from '../../redux/books/booksSlice';
+
+// const Form = () => {
+//   const dispatch = useDispatch();
+//   const [book, setBook] = useState({
+//     item_id: Date.now(),
+//     title: '',
+//     author: '',
+//     category: '',
+//   });
+//   const clearField = () => {
+//     setBook({
+//       item_id: Date.now(),
+//       title: '',
+//       author: '',
+//       category: '',
+//     });
+//   };
+
+//   const addNewBook = (e) => {
+//     e.preventDefault();
+//     if (book.title !== '') {
+//       dispatch(addBook(book));
+//       clearField();
+//     }
+//   };
+
+//   const handleChange = (e) => {
+//     e.preventDefault();
+//     setBook({
+//       ...book,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+
+//   return (
+//     <div className="form-container">
+//       <h2>ADD A NEW BOOK</h2>
+//       <form className="submit-form" onSubmit={addNewBook}>
+//         <input
+//           required
+//           name="title"
+//           className="input input-book"
+//           type="text"
+//           placeholder="Book Title"
+//           value={book.title}
+//           onChange={handleChange}
+//         />
+//         <input
+//           required
+//           name="author"
+//           className="input input-book"
+//           type="text"
+//           placeholder="Book author"
+//           value={book.author}
+//           onChange={handleChange}
+//         />
+//         <select
+//           name="category"
+//           value={book.category}
+//           className="input category-input"
+//           onChange={handleChange}
+//           required
+//         >
+//           <option value="Action">Action</option>
+//           <option value="Science Fiction">Science Fiction</option>
+//           <option value="Economy">Economy</option>
+//         </select>
+//         <button className="book-submit-btn" type="submit">
+//           Add Book
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Form;
+
+
+/* eslint-disable react/prop-types */
 import './form.css';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { addBook } from '../../redux/books/booksSlice';
+import { addBooks } from '../redux/books/booksSlice';
 
-const Form = () => {
+const Form = ({ booklength }) => {
   const dispatch = useDispatch();
-  const [book, setBook] = useState({
-    item_id: Date.now(),
-    title: '',
-    author: '',
-    category: '',
-  });
-  const clearField = () => {
-    setBook({
-      item_id: Date.now(),
-      title: '',
-      author: '',
-      category: '',
-    });
-  };
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
   const addNewBook = (e) => {
     e.preventDefault();
-    if (book.title !== '') {
-      dispatch(addBook(book));
-      clearField();
-    }
-  };
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    setBook({
-      ...book,
-      [e.target.name]: e.target.value,
-    });
+    dispatch(
+      addBooks({
+        id: booklength + 1,
+        title,
+        author,
+        category,
+      }),
+    );
+    setTitle('');
+    setAuthor('');
+    setCategory('');
   };
 
   return (
     <div className="form-container">
-      <h2>ADD A NEW BOOK</h2>
+      <h2>ADD NEW BOOK</h2>
       <form className="submit-form" onSubmit={addNewBook}>
         <input
           required
@@ -46,8 +117,8 @@ const Form = () => {
           className="input input-book"
           type="text"
           placeholder="Book Title"
-          value={book.title}
-          onChange={handleChange}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <input
           required
@@ -55,14 +126,14 @@ const Form = () => {
           className="input input-book"
           type="text"
           placeholder="Book author"
-          value={book.author}
-          onChange={handleChange}
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
         />
         <select
           name="category"
-          value={book.category}
+          value={category}
           className="input category-input"
-          onChange={handleChange}
+          onChange={(e) => setCategory(e.target.value)}
           required
         >
           <option value="Action">Action</option>
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,25 +1,88 @@
-import { createSlice } from '@reduxjs/toolkit';
-import bookList from '../../components/home/bookLists';
+// import { createSlice } from '@reduxjs/toolkit';
+// import bookList from '../../components/home/bookLists';
 
-const initialState = {
-  bookList: [...bookList],
-};
+// const initialState = {
+//   bookList: [...bookList],
+// };
 
-const bookSlice = createSlice({
-  name: 'book',
-  initialState,
-  reducers: {
-    addBook: (state, action) => {
-      const newBook = action.payload;
-      state.bookList = [...state.bookList, newBook];
-    },
-    removeBook: (state, action) => {
-      console.log('Action payload:', action.payload);
-      const itemId = action.payload;
-      state.bookList = state.bookList.filter((item) => item.item_id !== itemId);
-    },
+// const bookSlice = createSlice({
+//   name: 'book',
+//   initialState,
+//   reducers: {
+//     addBook: (state, action) => {
+//       const newBook = action.payload;
+//       state.bookList = [...state.bookList, newBook];
+//     },
+//     removeBook: (state, action) => {
+//       console.log('Action payload:', action.payload);
+//       const itemId = action.payload;
+//       state.bookList = state.bookList.filter((item) => item.item_id !== itemId);
+//     },
+//   },
+// });
+
+// export const { removeBook, addBook } = bookSlice.actions;
+// export default bookSlice.reducer;
+
+
+
+// eslint-disable-next-line max-len
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/mOn3TNtGJAgtwm69ygNI/books';
+
+export const getBookItems = createAsyncThunk(
+  'cart/getBookItems',
+  async (thunkAPI) => {
+    try {
+      const response = await axios(url);
+      const books = response.data;
+      return books;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  },
+);
+
+export const addBooks = createAsyncThunk('books/addBooks', async (book) => {
+  await axios.post(`${url}`, {
+    item_id: book.id,
+    title: book.title,
+    category: book.category,
+    author: book.author,
+  });
+  return book;
+});
+
+export const deleteBooks = createAsyncThunk(
+  'book/deleteBooks',
+  async (itemId) => {
+    await axios.delete(`${url}/${itemId}`);
+    return itemId;
+  },
+);
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState: [],
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // eslint-disable-next-line max-len
+      .addCase(getBookItems.fulfilled, (state, action) => Object.keys(action.payload).map((key) => ({
+        id: key,
+        title: action.payload[key][0].title,
+        author: action.payload[key][0].author,
+        category: action.payload[key][0].category,
+      })))
+
+      .addCase(addBooks.fulfilled, (state, action) => {
+        state.push(action.payload);
+      })
+      // eslint-disable-next-line max-len
+      .addCase(deleteBooks.fulfilled, (state, action) => state.filter((book) => book.id !== action.payload));
   },
 });
 
-export const { removeBook, addBook } = bookSlice.actions;
-export default bookSlice.reducer;
+export default booksSlice.reducer;
